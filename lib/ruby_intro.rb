671f1c32293301e36bb4fde313fc10f0f3849b92
Nitin <v3ntinty@tamu.edu>
2019-09-19 22:42:57 -0500

    Minor corrections due to comitting problems

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,60 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  s = 0
+  arr.each{ |a| s+=a}
+  return s
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return arr.length==0 ? 0: (arr.length == 1 ? arr[0]: arr.max(2).sum)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+    if (arr.combination(2).find{ |a,b| a+b == n})
+        return true
+    else
+        return false
+    end
 end
-
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+    return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+    return s =~ /\A(?=[^aeiou])(?=[a-z])/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    if s.length == 0
+        return false
+    end
+    sum = 0;
+    for i in 0..s.length-1
+        if s[i] != '0' && s[i] != '1'
+            return false
+        end
+        sum += ( s[i]=='1' ? 2**(s.length-1-i) : 0)
+    end
+    return sum%4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    attr_accessor :isbn, :price
+
+    def initialize (isbn,price)
+        # Instance variables
+        raise ArgumentError, 'Invalid Arguments' unless (isbn.length>0 && price > 0)
+        @isbn = isbn
+        @price = price
+    end
+
+    def price_as_string
+        return "$" + ('%.2f' % @price.to_s)
+    end
+
 end
